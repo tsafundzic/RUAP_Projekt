8cff34ccf1207bc2e0e5f5dfbc1f11b493b54f56
Tomislav <tsafundzic@gmail.com>
2017-08-25 20:02:23 +0200

    Added label, textbox and button

--- RUAP_ProcjenaSnageBetona/RUAP_ProcjenaSnageBetona/Form1.Designer.cs
+++ RUAP_ProcjenaSnageBetona/RUAP_ProcjenaSnageBetona/Form1.Designer.cs
@@ -28,12 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1_Cement = new System.Windows.Forms.Label();
+            this.label2_BlastFurnaceSlag = new System.Windows.Forms.Label();
+            this.textBox1_Cement = new System.Windows.Forms.TextBox();
+            this.textBox2_BlastFurnaceSlag = new System.Windows.Forms.TextBox();
+            this.textBox3_FlyAsh = new System.Windows.Forms.TextBox();
+            this.label3_FlyAsh = new System.Windows.Forms.Label();
+            this.textBox4_Water = new System.Windows.Forms.TextBox();
+            this.label4_Water = new System.Windows.Forms.Label();
+            this.textBox5_Superplasticizer = new System.Windows.Forms.TextBox();
+            this.label5_Superplasticizer = new System.Windows.Forms.Label();
+            this.textBox6_CoarseAggregate = new System.Windows.Forms.TextBox();
+            this.label6_CoarseAggregate = new System.Windows.Forms.Label();
+            this.textBox7_FineAggregate = new System.Windows.Forms.TextBox();
+            this.label7_FineAggregate = new System.Windows.Forms.Label();
+            this.textBox8_Day = new System.Windows.Forms.TextBox();
+            this.label8_Day = new System.Windows.Forms.Label();
+            this.label_Concretecompressivestrength = new System.Windows.Forms.Label();
+            this.label_result = new System.Windows.Forms.Label();
+            this.button_calculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1_Cement
+            // 
+            this.label1_Cement.AutoSize = true;
+            this.label1_Cement.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1_Cement.Location = new System.Drawing.Point(355, 88);
+            this.label1_Cement.Name = "label1_Cement";
+            this.label1_Cement.Size = new System.Drawing.Size(99, 15);
+            this.label1_Cement.TabIndex = 0;
+            this.label1_Cement.Text = "Cement [kg/m^3]";
+            // 
+            // label2_BlastFurnaceSlag
+            // 
+            this.label2_BlastFurnaceSlag.AutoSize = true;
+            this.label2_BlastFurnaceSlag.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2_BlastFurnaceSlag.Location = new System.Drawing.Point(295, 114);
+            this.label2_BlastFurnaceSlag.Name = "label2_BlastFurnaceSlag";
+            this.label2_BlastFurnaceSlag.Size = new System.Drawing.Size(159, 15);
+            this.label2_BlastFurnaceSlag.TabIndex = 1;
+            this.label2_BlastFurnaceSlag.Text = "Blast Furnace Slag [kg/m^3]";
+            // 
+            // textBox1_Cement
+            // 
+            this.textBox1_Cement.Location = new System.Drawing.Point(470, 87);
+            this.textBox1_Cement.Name = "textBox1_Cement";
+            this.textBox1_Cement.Size = new System.Drawing.Size(100, 20);
+            this.textBox1_Cement.TabIndex = 2;
+            // 
+            // textBox2_BlastFurnaceSlag
+            // 
+            this.textBox2_BlastFurnaceSlag.Location = new System.Drawing.Point(470, 113);
+            this.textBox2_BlastFurnaceSlag.Name = "textBox2_BlastFurnaceSlag";
+            this.textBox2_BlastFurnaceSlag.Size = new System.Drawing.Size(100, 20);
+            this.textBox2_BlastFurnaceSlag.TabIndex = 3;
+            // 
+            // textBox3_FlyAsh
+            // 
+            this.textBox3_FlyAsh.Location = new System.Drawing.Point(470, 139);
+            this.textBox3_FlyAsh.Name = "textBox3_FlyAsh";
+            this.textBox3_FlyAsh.Size = new System.Drawing.Size(100, 20);
+            this.textBox3_FlyAsh.TabIndex = 5;
+            // 
+            // label3_FlyAsh
+            // 
+            this.label3_FlyAsh.AutoSize = true;
+            this.label3_FlyAsh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label3_FlyAsh.Location = new System.Drawing.Point(360, 140);
+            this.label3_FlyAsh.Name = "label3_FlyAsh";
+            this.label3_FlyAsh.Size = new System.Drawing.Size(94, 15);
+            this.label3_FlyAsh.TabIndex = 4;
+            this.label3_FlyAsh.Text = "Fly Ash [kg/m^3]";
+            // 
+            // textBox4_Water
+            // 
+            this.textBox4_Water.Location = new System.Drawing.Point(470, 165);
+            this.textBox4_Water.Name = "textBox4_Water";
+            this.textBox4_Water.Size = new System.Drawing.Size(100, 20);
+            this.textBox4_Water.TabIndex = 7;
+            // 
+            // label4_Water
+            // 
+            this.label4_Water.AutoSize = true;
+            this.label4_Water.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label4_Water.Location = new System.Drawing.Point(363, 166);
+            this.label4_Water.Name = "label4_Water";
+            this.label4_Water.Size = new System.Drawing.Size(91, 15);
+            this.label4_Water.TabIndex = 6;
+            this.label4_Water.Text = "Water  [kg/m^3]";
+            // 
+            // textBox5_Superplasticizer
+            // 
+            this.textBox5_Superplasticizer.Location = new System.Drawing.Point(470, 191);
+            this.textBox5_Superplasticizer.Name = "textBox5_Superplasticizer";
+            this.textBox5_Superplasticizer.Size = new System.Drawing.Size(100, 20);
+            this.textBox5_Superplasticizer.TabIndex = 9;
+            // 
+            // label5_Superplasticizer
+            // 
+            this.label5_Superplasticizer.AutoSize = true;
+            this.label5_Superplasticizer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label5_Superplasticizer.Location = new System.Drawing.Point(310, 192);
+            this.label5_Superplasticizer.Name = "label5_Superplasticizer";
+            this.label5_Superplasticizer.Size = new System.Drawing.Size(144, 15);
+            this.label5_Superplasticizer.TabIndex = 8;
+            this.label5_Superplasticizer.Text = "Superplasticizer [kg/m^3]";
+            // 
+            // textBox6_CoarseAggregate
+            // 
+            this.textBox6_CoarseAggregate.Location = new System.Drawing.Point(470, 217);
+            this.textBox6_CoarseAggregate.Name = "textBox6_CoarseAggregate";
+            this.textBox6_CoarseAggregate.Size = new System.Drawing.Size(100, 20);
+            this.textBox6_CoarseAggregate.TabIndex = 11;
+            // 
+            // label6_CoarseAggregate
+            // 
+            this.label6_CoarseAggregate.AutoSize = true;
+            this.label6_CoarseAggregate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label6_CoarseAggregate.Location = new System.Drawing.Point(300, 218);
+            this.label6_CoarseAggregate.Name = "label6_CoarseAggregate";
+            this.label6_CoarseAggregate.Size = new System.Drawing.Size(154, 15);
+            this.label6_CoarseAggregate.TabIndex = 10;
+            this.label6_CoarseAggregate.Text = "Coarse Aggregate [kg/m^3]";
+            // 
+            // textBox7_FineAggregate
+            // 
+            this.textBox7_FineAggregate.Location = new System.Drawing.Point(470, 243);
+            this.textBox7_FineAggregate.Name = "textBox7_FineAggregate";
+            this.textBox7_FineAggregate.Size = new System.Drawing.Size(100, 20);
+            this.textBox7_FineAggregate.TabIndex = 13;
+            // 
+            // label7_FineAggregate
+            // 
+            this.label7_FineAggregate.AutoSize = true;
+            this.label7_FineAggregate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label7_FineAggregate.Location = new System.Drawing.Point(315, 244);
+            this.label7_FineAggregate.Name = "label7_FineAggregate";
+            this.label7_FineAggregate.Size = new System.Drawing.Size(139, 15);
+            this.label7_FineAggregate.TabIndex = 12;
+            this.label7_FineAggregate.Text = "Fine Aggregate [kg/m^3]";
+            // 
+            // textBox8_Day
+            // 
+            this.textBox8_Day.Location = new System.Drawing.Point(470, 269);
+            this.textBox8_Day.Name = "textBox8_Day";
+            this.textBox8_Day.Size = new System.Drawing.Size(100, 20);
+            this.textBox8_Day.TabIndex = 15;
+            // 
+            // label8_Day
+            // 
+            this.label8_Day.AutoSize = true;
+            this.label8_Day.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label8_Day.Location = new System.Drawing.Point(398, 270);
+            this.label8_Day.Name = "label8_Day";
+            this.label8_Day.Size = new System.Drawing.Size(56, 15);
+            this.label8_Day.TabIndex = 14;
+            this.label8_Day.Text = "Age [day]";
+            // 
+            // label_Concretecompressivestrength
+            // 
+            this.label_Concretecompressivestrength.AutoSize = true;
+            this.label_Concretecompressivestrength.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label_Concretecompressivestrength.Location = new System.Drawing.Point(366, 413);
+            this.label_Concretecompressivestrength.Name = "label_Concretecompressivestrength";
+            this.label_Concretecompressivestrength.Size = new System.Drawing.Size(209, 15);
+            this.label_Concretecompressivestrength.TabIndex = 16;
+            this.label_Concretecompressivestrength.Text = "Concrete compressive strength [Mpa]";
+            // 
+            // label_result
+            // 
+            this.label_result.AutoSize = true;
+            this.label_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label_result.Location = new System.Drawing.Point(413, 440);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(97, 46);
+            this.label_result.TabIndex = 17;
+            this.label_result.Text = "0.00";
+            // 
+            // button_calculate
+            // 
+            this.button_calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.button_calculate.Location = new System.Drawing.Point(421, 333);
+            this.button_calculate.Name = "button_calculate";
+            this.button_calculate.Size = new System.Drawing.Size(96, 39);
+            this.button_calculate.TabIndex = 18;
+            this.button_calculate.Text = "Calculate";
+            this.button_calculate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 592);
+            this.Controls.Add(this.button_calculate);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.label_Concretecompressivestrength);
+            this.Controls.Add(this.textBox8_Day);
+            this.Controls.Add(this.label8_Day);
+            this.Controls.Add(this.textBox7_FineAggregate);
+            this.Controls.Add(this.label7_FineAggregate);
+            this.Controls.Add(this.textBox6_CoarseAggregate);
+            this.Controls.Add(this.label6_CoarseAggregate);
+            this.Controls.Add(this.textBox5_Superplasticizer);
+            this.Controls.Add(this.label5_Superplasticizer);
+            this.Controls.Add(this.textBox4_Water);
+            this.Controls.Add(this.label4_Water);
+            this.Controls.Add(this.textBox3_FlyAsh);
+            this.Controls.Add(this.label3_FlyAsh);
+            this.Controls.Add(this.textBox2_BlastFurnaceSlag);
+            this.Controls.Add(this.textBox1_Cement);
+            this.Controls.Add(this.label2_BlastFurnaceSlag);
+            this.Controls.Add(this.label1_Cement);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1_Cement;
+        private System.Windows.Forms.Label label2_BlastFurnaceSlag;
+        private System.Windows.Forms.TextBox textBox1_Cement;
+        private System.Windows.Forms.TextBox textBox2_BlastFurnaceSlag;
+        private System.Windows.Forms.TextBox textBox3_FlyAsh;
+        private System.Windows.Forms.Label label3_FlyAsh;
+        private System.Windows.Forms.TextBox textBox4_Water;
+        private System.Windows.Forms.Label label4_Water;
+        private System.Windows.Forms.TextBox textBox5_Superplasticizer;
+        private System.Windows.Forms.Label label5_Superplasticizer;
+        private System.Windows.Forms.TextBox textBox6_CoarseAggregate;
+        private System.Windows.Forms.Label label6_CoarseAggregate;
+        private System.Windows.Forms.TextBox textBox7_FineAggregate;
+        private System.Windows.Forms.Label label7_FineAggregate;
+        private System.Windows.Forms.TextBox textBox8_Day;
+        private System.Windows.Forms.Label label8_Day;
+        private System.Windows.Forms.Label label_Concretecompressivestrength;
+        private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.Button button_calculate;
     }
 }
 
